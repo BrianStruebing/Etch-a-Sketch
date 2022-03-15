e4cb5151dd94d0d6c3a40ffc5bd5bfbefb413a0a
Brian Strübing <brian.struebing@gmx.de>
2022-03-15 12:18:59 +0100

    DOM Elements and Default Values
    selected all the DOM elements and set some Default values for the app.

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
+// Setting the Defaults
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+
+
+
+
 
 // Creating and selecting all the DOM elements we need
 const mainDiv = document.querySelector("main");
-const controlDiv = document.createElement("div");
-const gridDiv = document.createElement("div");
-const testHeading = document.createElement("h1");
+// const controlDiv = document.createElement("div");
+const gridDiv = document.querySelector(".gridDiv")
+// const testHeading = document.createElement("h1");
+const clear = document.getElementById("clearBtn");
+const erease = document.getElementById("earserBtn");
+const rainbowMode = document.getElementById("rainbowBtn");
+const colorMode = document.getElementById("colorBtn");
 
 
 
-testHeading.textContent = "Hello";
 
 
-controlDiv.classList.add("controlDiv");
 
-controlDiv.appendChild(testHeading);
-mainDiv.appendChild(controlDiv);
+// testHeading.textContent = "Hello";
+
+
+// controlDiv.classList.add("controlDiv");
+
+// controlDiv.appendChild(testHeading);
+// mainDiv.appendChild(controlDiv);
